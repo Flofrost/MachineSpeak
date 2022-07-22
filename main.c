ab7b3b99a775e6c9aec6a06b1498cbed8078e655
Flofrost <ballagerflorian@gmail.com>
2022-07-22 21:21:37 +0200

    core mostly done, files gettings written fine, need to sample letters

--- main.c
+++ main.c
@@ -1,10 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define ok printf("ok\n");
 #define SOUND_PATH "./audio"
+#define system(x) (system(x) >> 8)
 
+// char outfilename[256] = "/tmp/machinespeak/out.wav";
+char outfilename[256] = "audio/out.wav";
 const char sounds[40] = "0123456779abcdefghijklmopqrstuvwxyz_.-";
-const unsigned char wavHeader[40] = {82,73,70,70,40,74,1,0,87,65,86,69,102,109,116,32,16,0,0,0,1,0,1,0,68,172,0,0,136,88,1,0,2,0,16,0,100,97,116,97};
+const unsigned char wavHeader[44] = {0x52,0x49,0x46,0x46,0x00,0x00,0x00,0x00,0x57,0x41,0x56,0x45,0x66,0x6d,0x74,0x20,0x10,0x00,0x00,0x00,0x01,0x00,0x01,0x00,0x44,0xac,0x00,0x00,0x88,0x58,0x01,0x00,0x02,0x00,0x10,0x00,0x64,0x61,0x74,0x61,0x00,0x00,0x00,0x00};
 
 void help(){
 
@@ -60,12 +64,52 @@ void playMessage(char* message){
 }
 
 void writeFile(char sound){
-    char command[100];
+
+    char command[512];
+    char infilename[256];
     sprintf(command, "exit $(ls %s | grep -P \"%c\\.\\d\\.wav\" | wc -l)",SOUND_PATH,sound);
-    int nfiles = system(command)>>8;
-    FILE* test = fopen(SOUND_PATH"/test.wav","w");
-    fwrite(wavHeader,1,40,test);
-    fclose(test);
+    int nfile = system(command);
+    nfile = rand() % nfile;
+    sprintf(infilename,"%s/%c.%d.wav",SOUND_PATH,sound,nfile);
+
+    sprintf(command,"[ -s \"%s\" ]",outfilename);
+    if(system(command)){
+        sprintf(command,"mkdir -p $(dirname %s)",outfilename);
+        system(command);
+        FILE* outfile = fopen(outfilename,"w");
+        fwrite(wavHeader,1,40,outfile);
+        fclose(outfile);
+    }
+
+    FILE* outfile = fopen(outfilename,"a");
+    FILE* infile = fopen(infilename,"r");
+    fseek(infile,40,SEEK_SET);
+    unsigned char buff[256];
+    int nbytes = 256;
+
+    while(!feof(infile)){
+        nbytes = fread(buff,1,nbytes,infile);
+        fwrite(buff,1,nbytes,outfile);
+    }
+
+    fclose(infile);
+    fclose(outfile);
+
+}
+
+void endFile(){
+    int32_t size = 0;
+    FILE* outfile = fopen(outfilename,"r+");
+    while(fgetc(outfile) != EOF) size++;
+    
+    size -= 8;
+    fseek(outfile,4,SEEK_SET);
+    fwrite(&size,4,1,outfile);
+    size -= 36;
+    fseek(outfile,40,SEEK_SET);
+    fwrite(&size,4,1,outfile);
+
+    fclose(outfile);
 }
 
 int main(int argc, char** argv){
@@ -73,9 +117,16 @@ int main(int argc, char** argv){
     if(argc > 1){
         
         char* msgout = (char*) malloc(msglen(argv[1]) * sizeof(char));
+        char command[512];
+
         transcriptMessage(0,argv[1],msgout);
-        printf("%s\n",msgout);
-        writeFile('1');
+
+        sprintf(command,"rm %s",outfilename);
+        system(command);
+
+        for(int i = 0 ; argv[1][i] ; i++) writeFile(argv[1][i]);
+
+        endFile();
 
     }else help();
 
